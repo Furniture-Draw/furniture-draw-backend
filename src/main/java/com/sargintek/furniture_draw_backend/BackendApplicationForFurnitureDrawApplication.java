882630cb3ef38bb1749e15ac7e225c7f08b71b69
Yusuf Sargın <35580473+yusufsargin@users.noreply.github.com>
2024-11-27 22:50:16 +0300

    first commit

---
+++ src/main/java/com/sargintek/furniture_draw_backend/BackendApplicationForFurnitureDrawApplication.java
@@ -0,0 +1,13 @@
+package com.sargintek.furniture_draw_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplicationForFurnitureDrawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplicationForFurnitureDrawApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sargintek/furniture_draw_backend/BackendApplicationForFurnitureDrawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sargintek.furniture_draw_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationForFurnitureDrawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
